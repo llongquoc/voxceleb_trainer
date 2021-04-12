fd81f8a43fd5c1096266df37fd3a15a06981421e
Le Long Quoc <llongquoc1999@gmail.com>
2021-04-12 22:02:13 +0700

    Fix minor bug.

--- SpeakerNet.py
+++ SpeakerNet.py
@@ -23,36 +23,6 @@ class WrappedModel(nn.Module):
         return self.module(x, label)
 
 
-class SpeakerNetCPU(SpeakerNet):
-
-    def __init__(self, model, optimizer, trainfunc, nPerSpeaker, **kwargs):
-        super(SpeakerNet, self).__init__();
-
-        SpeakerNetModel = importlib.import_module('models.'+model).__getattribute__('MainModel')
-        self.__S__ = SpeakerNetModel(**kwargs);
-
-        LossFunction = importlib.import_module('loss.'+trainfunc).__getattribute__('LossFunction')
-        self.__L__ = LossFunction(**kwargs);
-
-        self.nPerSpeaker = nPerSpeaker
-
-    def forward(self, data, label=None):
-
-        data    = data.reshape(-1,data.size()[-1])
-        outp    = self.__S__.forward(data)
-
-        if label == None:
-            return outp
-
-        else:
-
-            outp    = outp.reshape(self.nPerSpeaker,-1,outp.size()[-1]).transpose(1,0).squeeze(1)
-
-            nloss, prec1 = self.__L__.forward(outp,label)
-
-            return nloss, prec1
-
-
 class SpeakerNet(nn.Module):
 
     def __init__(self, model, optimizer, trainfunc, nPerSpeaker, **kwargs):
@@ -83,6 +53,36 @@ class SpeakerNet(nn.Module):
             return nloss, prec1
 
 
+class SpeakerNetCPU(SpeakerNet):
+
+    def __init__(self, model, optimizer, trainfunc, nPerSpeaker, **kwargs):
+        super(SpeakerNet, self).__init__();
+
+        SpeakerNetModel = importlib.import_module('models.'+model).__getattribute__('MainModel')
+        self.__S__ = SpeakerNetModel(**kwargs);
+
+        LossFunction = importlib.import_module('loss.'+trainfunc).__getattribute__('LossFunction')
+        self.__L__ = LossFunction(**kwargs);
+
+        self.nPerSpeaker = nPerSpeaker
+
+    def forward(self, data, label=None):
+
+        data    = data.reshape(-1,data.size()[-1])
+        outp    = self.__S__.forward(data)
+
+        if label == None:
+            return outp
+
+        else:
+
+            outp    = outp.reshape(self.nPerSpeaker,-1,outp.size()[-1]).transpose(1,0).squeeze(1)
+
+            nloss, prec1 = self.__L__.forward(outp,label)
+
+            return nloss, prec1
+
+            
 class ModelTrainer(object):
 
     def __init__(self, speaker_model, optimizer, scheduler, gpu, mixedprec, **kwargs):
